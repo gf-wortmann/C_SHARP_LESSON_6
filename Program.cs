dc16bf087ed58638453acfb762cb667963f2a3d0
George Wortmann <gf.wortmann@gmail.com>
2022-11-21 22:32:47 +0300

    task 43 classes 1

--- Program.cs
+++ Program.cs
@@ -2,77 +2,53 @@
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
+Line L = new Line (5.0, 11.5);
+Line M = L.GetLine();
+
+Console.WriteLine($"M.Angular: {M.A()}, M.Displacement: {M.D()} ");
 
 
-double [] GetLine (int numberInOrder, int angularCoefficientIndex, int displacementIndex)
+class Line
 {
-    double[] result = new double [2];
-/// Getting angular coefficient
-    Console.Clear();
-    Console.WriteLine($"For Line #{numberInOrder} enter ");
-    Console.Write("angular coefficient:  ");
-    result [0] = double.Parse(Console.ReadLine()!);
-
-/// Getting displaceent    
-    Console.WriteLine("");
-    Console.Write("and displacement:  ");
-    result [1] = double.Parse(Console.ReadLine()!);
-
-///returning line data    
-    return result;  
-}
-
-
-bool AreParallel (double angularCoefficientOne, double angularCoefficientTwo)
-{
-    return (angularCoefficientOne == angularCoefficientTwo);
-}
-
-double [] GetIntersectionPoint ( double [] lineOne, double [] lineTwo)
-{
-    double [] Point = new double [2];
-    int angularCoefficientIndex = 0;
-    int displacementIndex = 1;
-    // Kramer formula
+    private double angularCoefficient, displacement;
     
-    double a, b, c, d;
-    a = lineOne [0];
-    b = lineTwo [0];
-    c = lineOne [1];
-    d = lineTwo [1];
+    public Line()
+    {
+        angularCoefficient  = 0;
+        displacement        = 0;
+    }
 
-//    Console.WriteLine($"a = {a}. b = {b}, c = {c}, d = {d}");
+    public Line (double angularCoefficient, double displacement)
+    {
+        this.angularCoefficient = angularCoefficient;
+        this.displacement       = displacement;
+    }
 
-    Point[angularCoefficientIndex] = 
-                                    (d - c)
-                                    /
-                                    (a - b);
-    Point[displacementIndex] = 
-                            (a * d - b * c)
-                            /                    
-                            (a - b);
+    public void SetLine (double angularCoefficient, double displacement)
+    {
+        this.angularCoefficient = angularCoefficient;
+        this.displacement       = displacement;
+    }
 
-//    Console.WriteLine($"!Point is on: {Point[0]}, {Point[1]}");
-    return Point;
+    public Line GetLine ()
+    {
+        return this;
+    }
+
+    public double A ()
+    {
+        return angularCoefficient;
+    }
+
+    public double D ()
+    {
+        return displacement;
+    }
+}
+
+class Geometry
+{
+    
 }
 
 
-double [] lineOne, lineTwo, intersectionPoint = new double[2];
-int angularCoefficientIndex = 0;
-int displacementIndex = 1;
-
-lineOne = GetLine(1, angularCoefficientIndex, displacementIndex);
-lineTwo = GetLine(2, angularCoefficientIndex, displacementIndex);
-
-if (!AreParallel (lineOne[angularCoefficientIndex], lineTwo[angularCoefficientIndex])) 
-{
-    intersectionPoint = GetIntersectionPoint (lineOne, lineTwo);
-    Console.WriteLine($"Point of intersection is on: {intersectionPoint[0]:f2}, {intersectionPoint[1]:f2}");
-} else
-{
-    Console.WriteLine($"Lines are parallel, they haven't any intersection");
-}
-
-
-//int [] inputNumbers;
-//Console.WriteLine($"There were {positiveNumbersCount (getNumbers())} numbers large than zero entered.");
